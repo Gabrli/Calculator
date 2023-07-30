40445378288dd2cbce81db8db4d40dfaad8469e2
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-30 19:51:56 +0200

    Add symbols and buttons

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,3 @@
 const symbolsAndNumberContainer = document.querySelector('.symbols-and-numbers-container');
+let test = '5 * 2 + 10';
+console.log(eval(test));
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 const symbolsAndNumberContainer:HTMLElement = document.querySelector('.symbols-and-numbers-container')
 
 
-//inerHtml
+let test:string = '5 * 2 + 10'
+console.log(eval(test))
