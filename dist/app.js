5a101d4de11327da6ae3f0d1e82ec4a91d4e6330
GabrielDev <gabrys.wisniewski@op.pl>
2023-07-31 09:41:46 +0200

    Logic add

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,32 @@
-const symbolsAndNumberContainer = document.querySelector('.symbols-and-numbers-container');
-let test = '5 * 2 + 10';
-console.log(eval(test));
+const numbers = document.querySelectorAll(".btn");
+const operators = document.querySelectorAll(".operators");
+const resultsTile = document.querySelector(".results");
+let result = "";
+const displaySymbols = (e) => {
+    const resultBtn = document.querySelector("#equel");
+    const clearBtn = document.querySelector("#clear");
+    const delateBtn = document.querySelector("#delate");
+    const target = e.target;
+    let updateValue = (resultsTile.innerHTML += target.innerHTML);
+    resultBtn.addEventListener("click", () => displayResult(updateValue));
+    clearBtn.addEventListener("click", clearSymbols);
+    delateBtn.addEventListener("click", () => { });
+    console.log(delateBtn);
+};
+const clearSymbols = () => {
+    resultsTile.innerHTML = "";
+};
+const displayResult = (value) => {
+    resultsTile.innerHTML = "";
+    if (value === "") {
+        return;
+    }
+    result = eval(value);
+    resultsTile.innerHTML = result;
+};
+numbers.forEach((number) => {
+    number.addEventListener("click", displaySymbols);
+});
+operators.forEach((operator) => {
+    operator.addEventListener("click", displaySymbols);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,38 @@
-const symbolsAndNumberContainer:HTMLElement = document.querySelector('.symbols-and-numbers-container')
+const numbers: NodeList = document.querySelectorAll(".btn");
+const operators: NodeList = document.querySelectorAll(".operators");
+const resultsTile: HTMLElement = document.querySelector(".results");
 
+let result = "";
+const displaySymbols = (e: Event) => {
+  const resultBtn: HTMLElement = document.querySelector("#equel");
+  const clearBtn: HTMLElement = document.querySelector("#clear");
+  const delateBtn: HTMLElement = document.querySelector("#delate");
+  const target = e.target as HTMLButtonElement;
+  let updateValue: string = (resultsTile.innerHTML += target.innerHTML);
 
-let test:string = '5 * 2 + 10'
-console.log(eval(test))
+  resultBtn.addEventListener("click", () => displayResult(updateValue));
+  clearBtn.addEventListener("click", clearSymbols);
+  delateBtn.addEventListener("click", () => {});
+  console.log(delateBtn);
+};
+
+const clearSymbols = () => {
+  resultsTile.innerHTML = "";
+};
+
+const displayResult = (value: string) => {
+  resultsTile.innerHTML = "";
+  if (value === "") {
+    return;
+  }
+
+  result = eval(value);
+  resultsTile.innerHTML = result;
+};
+
+numbers.forEach((number) => {
+  number.addEventListener("click", displaySymbols);
+});
+operators.forEach((operator) => {
+  operator.addEventListener("click", displaySymbols);
+});
